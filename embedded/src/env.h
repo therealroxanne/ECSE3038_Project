1af1f0a863984e19a7a2d8c90930e6a7366049f8
therealroxanne <roxannedaley054@gmail.com>
2023-05-19 10:21:42 -0500

    endpoint added

--- embedded/src/env.h
+++ embedded/src/env.h
@@ -1,4 +1,4 @@
 const char * WIFI_SSID="CWC-5316377";
 const char * WIFI_PASS="qnfmgxRgp3mh";
-const char * ENDPOINT="";
+const char * ENDPOINT="https://iot-smarthub.onrender.com";
 const char * API_KEY= "";
